354b7add5d37c6499e6f3080e3b3d0bb2970270b
B Y <122250606+virus1019@users.noreply.github.com>
2023-04-05 15:17:56 -0400

    completed -added row containers and changed flex properties, clear now removes row containers

--- etch.js
+++ etch.js
@@ -1,23 +1,64 @@
 let mouseDown = false;
 document.body.onmousedown = () => mouseDown = true;
 document.body.onmouseup = () => mouseDown = false;
+let width = 16;
+let height = 16;
 
-function createGrid(){
-    const WIDTH = 16;
-    const HEIGHT = 16;
-    const container = document.querySelector('.container')
-    for (i = 0; i < WIDTH; i++){
-        for (j = 0; j < HEIGHT; j++){
-            const grid = document.createElement('div');
-            grid.classList.add('block');
-            container.appendChild(grid);
-        }
-        const lineBreak = document.createElement('div');
-        lineBreak.classList.add('break');
-        container.appendChild(lineBreak);
+let clear = document.querySelector('.clearGrid')
+clear.addEventListener('click', clearGrid());
+
+let create = document.querySelector('.createGrid')
+create.addEventListener('click', () => {
+    let arrHeightWidth = getUserInput();
+    height = arrHeightWidth[0];
+    width = arrHeightWidth[1];
+    
+    if (Number.isInteger(height) === true && Number.isInteger(width) === true && height >= 1 && height <= 100 && width >= 1 && width <= 100){
+        clearGrid();
+        let errorMessage = "";
+        document.getElementById("errorCode").textContent = errorMessage;
+        createGrid(width, height);
+        return arrHeightWidth;
     }
-    const gridBlock = document.querySelectorAll('.block')
-    gridBlock.forEach((block) => {
+    else {
+        let errorMessage = "Invalid Input. Please enter an integer between 1 and 100";
+        document.getElementById("errorCode").textContent = errorMessage;
+        return;
+    }
+})
+
+function clearGrid(){
+    document.querySelectorAll('.rowContainer').forEach(e => e.remove());
+}
+
+function getUserInput(){
+    let arrayHeightWidth = [];
+    inputHeight = document.querySelector('.width');
+    let inputValueHeight = Number(inputHeight.value);
+    arrayHeightWidth[0] = inputValueHeight;
+    
+    inputWidth = document.querySelector('.height');
+    let inputValueWidth = Number(inputWidth.value);
+    arrayHeightWidth[1] = inputValueWidth;
+    return arrayHeightWidth;
+}
+
+
+function createGrid(WIDTH, HEIGHT){
+    const container = document.querySelector('#container')
+    for (i = 0; i < WIDTH; i++){
+        const rowContainer = document.createElement('div');
+        rowContainer.classList.add('rowContainer');
+        container.appendChild(rowContainer);
+        for (j = 0; j < HEIGHT; j++){
+            const miniContainer = document.querySelectorAll('.rowContainer');
+            block = document.createElement('div');
+            block.classList.add('block');
+            miniContainer[i].appendChild(block);
+        }
+    }
+    const allBlocks = document.querySelectorAll('.block')
+    allBlocks.forEach((block) => {
         block.addEventListener('mouseover', draw);
         block.addEventListener('mousedown', draw);
     })
@@ -28,6 +69,4 @@ function draw(e) {
     e.target.style.background = 'blue';
 }
 
-createGrid();
-//mouseover && mousedown
-//if (e.type === 'mouseover' && !mouseDown) return
+createGrid(width, height);
